6d302ddd92ae4e72c47f3f862421f6d1c3812725
Elias Dominguez <edominguez.ing@gmail.com>
2020-05-16 17:45:25 -0400

    third commint desafios_ruby

---
+++ mayor_de_3.rb
@@ -0,0 +1,7 @@
+#lee datos
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+c = ARGV[2].to_i
+#imprime en pantalla el número mayor.
+datos= [a,b,c]
+puts datos.max
---
+++ mayor_de_4.rb
@@ -0,0 +1,8 @@
+#lee datos
+a = ARGV[0].to_i
+b = ARGV[1].to_i
+c = ARGV[2].to_i
+d = ARGV[3].to_i
+#imprime en pantalla el número mayor.
+datos= [a,b,c,d]
+puts datos.max
