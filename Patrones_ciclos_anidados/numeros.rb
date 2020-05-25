3e2ec021139fd4404afc44de1e739d728e995d57
Elias Dominguez <edominguez.ing@gmail.com>
2020-05-24 20:46:18 -0400

    eleventh commit desafios ruby

--- Patrones_ciclos_anidados/numeros.rb
+++ Patrones_ciclos_anidados/numeros.rb
@@ -1,7 +1,6 @@
  n=ARGV[0].to_i
 (n+1).times {|i|
-    1.upto(i) {|j| print j 
-    }
+    1.upto(i) {|j| print j }
    print " "
 }
 print "\n"
--- Patrones_ciclos_anidados/patrones.rb
+++ Patrones_ciclos_anidados/patrones.rb
@@ -1,44 +1,33 @@
 def letra_o(n)
     # inicio
-    n.times do |i|
-    print "*"
-    end
+    n.times { |i| print "*"}
     print "\n"
     #centro
     (n-2).times do
         print "*"
-        (n-2).times do |i|
-            print " "
-        end
+        (n-2).times {|i| print " "}
         print "*"
         print "\n"
     end
     # final
-    n.times do |i|
-    print "*"
-    end
+    n.times { |i| print "*"}
+    print "\n"
 end
 #letra_o(ARGV[0].to_i)
 
 def letra_i(n)
     # inicio
-    n.times do |i|
-    print "*"
-    end
+    n.times { |i| print "*"}
     print "\n"
     #centro
     (n-2).times do
         print " "*(n/2)
-        (1).times do |i|
-            print "*"   
-        end
+        print "*" 
         print " "
         print "\n"
     end
     # final
-    n.times do |i|
-    print "*"
-    end
+    n.times {|i| print "*"}
     print"\n"
 end
 #letra_i(ARGV[0].to_i)
@@ -46,27 +35,19 @@ end
 
 def letra_z(n)
  # inicio
-    n.times do |i|
-        print "*"
-    end
+    n.times {|i| print "*"}
     print "\n"
     #centro
     (n-2).times do |i|
         print " "
-        (n-i).downto(4) do
-        print " "
-        end
+        (n-i).downto(4) { print " "}
         1.times {|j| print "*"}
-        (n-i).downto(4) do
-            print " "
-        end
+        (n-i).downto(4) {print " "}
         print " "
         print "\n"
     end
     # final
-    n.times do |i|
-    print "*"
-    end
+    n.times {|i| print "*"}
     print"\n"
 end
 #letra_z(ARGV[0].to_i)
@@ -86,14 +67,12 @@ def letra_x(x)
     end
     #abajo
     (n).times do |i| 
-        (n-i).times do |j|
-            print " "
-        end
+        (n-i).times {|j| print " "}
         print "*"
         ((n-i)+1).upto(x/2) do
         print "  "        
         end
-        if i>0
+        if i>0 
             print" *"
         end
         print "\n"     
@@ -103,27 +82,19 @@ end
 
 def numero_cero(n)
     # inicio
-    n.times do |i|
-        print "*"
-    end
+    n.times {|i| print "*"}
         print "\n"
         #centro
     (n-2).times do |i|
         print "*"
-        (n-i).upto(n-1) do |i|
-            print " " 
-        end
+        (n-i).upto(n-1) {|i| print " "}
         print "*"
-        (n-i).downto(n-(n-4)) do |i|
-            print " " 
-        end
+        (n-i).downto(n-(n-4)) {|i| print " "}
         print "*"
         print "\n"
     end
         # final
-        n.times do |i|
-        print "*"
-    end 
+        n.times {|i| print "*"}
     print "\n"
 end
 #numero_cero(ARGV[0].to_i)
@@ -131,13 +102,9 @@ end
 def navidad(n)
     # inicio
     (n-1).times do |i| 
-        (n-i).times do |j|
-            print " "
-        end
+        (n-i).times {|j| print " "}
         print "*"
-        (n-i).upto(n-1) do
-                (1).times {|j| print " *"}
-            end    
+        (n-i).upto(n-1) { print " *"}
         print " "  
         print "\n"     
     end
