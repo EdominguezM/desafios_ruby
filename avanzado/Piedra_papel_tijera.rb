4e4716e11ce07d40e40762f46e3f6675fefd75dd
Elias Dominguez <edominguez.ing@gmail.com>
2020-05-30 18:06:01 -0400

    thirteenth commit desafios ruby

--- avanzado/Piedra_papel_tijera.rb
+++ avanzado/Piedra_papel_tijera.rb
@@ -1,10 +1,10 @@
 
 def menu
-    puts
-    puts '1) Piedra'
-    puts '2) Papel'
-    puts '3) Tijeras'
-    puts '4) Salir'
+    print"\n"
+    print "1) Piedra\n"
+    print "2) Papel\n"
+    print "3) Tijeras\n"
+    print "4) Salir\n"
 end
 status = {1=>"piedra",2=> "papel",3=>"tijera",4=>"Salir"}
 print "\n*** Bienvenidos al Cachipún *** \n\n"
@@ -15,42 +15,36 @@ puts
 #jugador I
 jugador1 = gets.chomp.to_i
 puts
-if jugador1 != 1..3
-    if jugador1 > 4 || jugador1 < 1
+if jugador1 != 1..3 || jugador1==4
+    while jugador1 > 4 || jugador1 < 1
         puts "ingrese valor correcto" 
         menu
         puts
         jugador1 = gets.chomp.to_i
         puts
-    elsif jugador1 ==4
-        exit
     end
     if jugador1==4 
         exit 
-    end        
+    end
 end
-
 #jugador II
 puts 'Turno jugador II'
 menu
 puts
 jugador2 = gets.chomp.to_i
 puts
-if jugador2 != 1..3
-    if jugador2 > 4 || jugador1 < 1
+if jugador2 != 1..3 || jugador1==4
+    while jugador2 > 4 || jugador1 < 1
         puts "ingrese valor correcto" 
         menu
         puts
         jugador2 = gets.chomp.to_i
         puts
-        elsif jugador1 ==4
-        exit
     end
     if jugador2==4 
         exit 
     end
 end
-
 #Juego
  gamer1= status.fetch(jugador1)
  puts "Jugador I: #{gamer1}"
