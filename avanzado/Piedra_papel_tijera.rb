51ae4b6ce6b02ae3bc24359a33c5866cb0ae1b3e
Elias Dominguez <edominguez.ing@gmail.com>
2020-05-30 18:20:58 -0400

    fourtheen commit

--- avanzado/Piedra_papel_tijera.rb
+++ avanzado/Piedra_papel_tijera.rb
@@ -1,4 +1,3 @@
-
 def menu
     print"\n"
     print "1) Piedra\n"
@@ -6,45 +5,39 @@ def menu
     print "3) Tijeras\n"
     print "4) Salir\n"
 end
+def valido(jugado)
+    if jugado != 1..3 || jugado==4
+        while jugado > 4 || jugado < 1
+            puts "ingrese valor correcto" 
+            menu
+            puts
+            jugado = gets.chomp.to_i
+            puts
+        end
+        if jugado==4 
+            exit 
+        end
+    end
+end
 status = {1=>"piedra",2=> "papel",3=>"tijera",4=>"Salir"}
 print "\n*** Bienvenidos al Cachipún *** \n\n"
 #imprimir en pantalla menú
 print   "Turno Jugador I\n"
-menu
+menu 
 puts
 #jugador I
 jugador1 = gets.chomp.to_i
+jugado= jugador1
+valido(jugado)
 puts
-if jugador1 != 1..3 || jugador1==4
-    while jugador1 > 4 || jugador1 < 1
-        puts "ingrese valor correcto" 
-        menu
-        puts
-        jugador1 = gets.chomp.to_i
-        puts
-    end
-    if jugador1==4 
-        exit 
-    end
-end
 #jugador II
 puts 'Turno jugador II'
 menu
 puts
 jugador2 = gets.chomp.to_i
+jugado= jugador2
+valido(jugado)
 puts
-if jugador2 != 1..3 || jugador1==4
-    while jugador2 > 4 || jugador1 < 1
-        puts "ingrese valor correcto" 
-        menu
-        puts
-        jugador2 = gets.chomp.to_i
-        puts
-    end
-    if jugador2==4 
-        exit 
-    end
-end
 #Juego
  gamer1= status.fetch(jugador1)
  puts "Jugador I: #{gamer1}"
