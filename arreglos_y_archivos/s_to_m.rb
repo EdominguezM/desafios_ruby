355feadd5f69d2bb8f48522174d2b00685d12e74
Elias Dominguez <edominguez.ing@gmail.com>
2020-05-31 15:17:26 -0400

    seventheen commit sixtheen  desafios ruby

--- arreglos_y_archivos/s_to_m.rb
+++ arreglos_y_archivos/s_to_m.rb
@@ -3,13 +3,14 @@ def to_minutes(seconds)
     list=[]
     n= seconds.count
     n.times do |i| 
-        if seconds[i] >= 90 
-            list.push seconds[i]/60.to_i
-        else  
-           list.delete seconds[i]
-        end
+        t= Time.at(seconds[i]).min
+        list.push t if t >= 1 else list.delete seconds[i]
+        #print m if seconds[i] > 1
+        #     list.push seconds[i]/60.to_i
+        # else  
+        #    list.delete seconds[i]
+        # end
     end
-    list
-    
+    list 
 end
-puts to_minutes(seconds) 
+print "minutes = #{to_minutes(seconds)}\n" 
